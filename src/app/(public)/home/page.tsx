7619a40aaae96d2cff5452df3d6044c96c9d42bc
JosuePorras <josueporrasrojas8@gmail.com>
2026-04-02 02:19:03 -0600

    feat(home): delete featuredSection

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -1,15 +1,13 @@
 import HeroSection from "@/components/home/HeroSection";
 import SummarySection from "@/components/home/SummarySection";
 import StackSection from "@/components/home/StackSections";
-import FeaturedProjectsSection from "@/components/home/FeaturedSection";
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
-      <SummarySection />
       <StackSection />
-      <FeaturedProjectsSection />
+      <SummarySection />
     </main>
   );
 }
